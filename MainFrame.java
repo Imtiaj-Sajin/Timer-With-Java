7197b6547eb774f19c698e7ba422cfa34ff5e32a
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-08-30 00:14:21 +0600

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class MainFrame {
     public static void main(String[] args) {
         JFrame frame= new JFrame();
+        frame.setBounds(660,400,600,400);
     }
     
 }
